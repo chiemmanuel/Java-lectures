3a6ef315d752a73fbe033c3fb758452a76a00549
chiemmanuel <123269646+chiemmanuel@users.noreply.github.com>
2023-10-19 20:36:22 +0200

    Update Launcher.java

--- SchoolManagement/src/launcher/Launcher.java
+++ SchoolManagement/src/launcher/Launcher.java
@@ -1,46 +1,45 @@
+
 package launcher;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import datamodel.Admin;
 import datamodel.Courses;
-import datamodel.Enrollement;
+import datamodel.Enrollment;
 import datamodel.Instructors;
 import datamodel.Students;
+import datamodel.Payment;
+import datamodel.Subjects;
+import service.PayService;
 
 public class Launcher {
 
-	public static void main(String[] a) {
-		Admin admin = new Admin();
-		admin.setAdmiId(1);
-		admin.setPassword("password");
-		admin.setUsername("Admin1");
-		
-		Courses course = new Courses();
-		course.setCourseID(1);
-		course.setCourseName("first course");
-		course.setCourseDuration("3 weeks");
-		
-		Enrollement enrollement = new Enrollement();
-		enrollement.setDate(null);
-		enrollement.setDetails(null);
-		enrollement.setEnrollementStatus(null);
-		
-		Instructors instructor = new Instructors();
-		instructor.setInstructorEmail("teacher@email.com");
-		instructor.setInstructorId(1);
-		instructor.setInstructorName("teacher");
-	
-		Students student = new Students();
-		student.setStudId("1"); // Set the student ID as a string
-		student.setName("student1");
-		student.setAge("20"); // Set the age as a string
-		student.setDepartment("Computer Science");
-		student.setYear("2nd"); // Set the year as a string
-		student.setCourse(List.of("Math", "Physics", "Programming")); // Set the courses as a List of strings
-		student.setFee(1000.0); // Set the fee as a double
-		student.setPaid("Yes");
-		
+	public static void printStudents(List<Students> list) {
+		for (int i = 0; i < list.size(); i++) {
+		    Students el = list.get(i);
+		    System.out.printf("ID: %d Name: %s Age: %d Courses: %s Department: %s Year: %d Fee: %.2f Paid: %.2f%n",
+		            el.getStudId(), el.getName(), el.getAge(), el.getCourse(), el.getDepartment(),
+		            el.getYear(), el.getFee(), el.getPaid());
+		}
 	}
-	
+
+	public static void main(String[] args) throws ParseException {
+		PayService payservice = new PayService();
+		try {
+			List<Students> student_list = payservice.read();
+			System.out.println("Before sort: \n");
+			printStudents(student_list);
+			payservice.sortByYear(student_list);
+			System.out.println("\n\n After sort: \n");
+			printStudents(student_list);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
+
